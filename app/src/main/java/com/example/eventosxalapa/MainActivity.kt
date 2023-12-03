1d51c75a30c69c58fe56da91355d99edf9d7e5c7
aaronMan12 <111460458+aaronMan12@users.noreply.github.com>
2023-12-03 17:23:33 -0600

    HamburguerMenu

--- app/src/main/java/com/example/eventosxalapa/MainActivity.kt
+++ app/src/main/java/com/example/eventosxalapa/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.eventosxalapa
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.eventosxalapa.Models.EventosProvider
 import com.example.eventosxalapa.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var toggle: ActionBarDrawerToggle
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +25,27 @@ class MainActivity : AppCompatActivity() {
         val btnHoy = binding.btnHoy
         val btnSemana = binding.btnSemana
         val btnMes = binding.btnMes
+        val btnGratis = binding.button
+        var btnPaga = binding.button2
+
+        val drawerLayout : DrawerLayout = binding.drawerLayout
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this@MainActivity,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(this@MainActivity,"Accion",Toast.LENGTH_LONG).show()
+            }
+            true
+        }
+
+
+
 
         btnHoy.setOnClickListener {
             cargarLista()
@@ -32,11 +59,24 @@ class MainActivity : AppCompatActivity() {
             cargarListaMes()
         }
 
+        btnGratis.setOnClickListener{
+            cargarListaGratis()
+        }
 
+        btnPaga.setOnClickListener{
+            cargarListaPaga()
 
+        }
 
 
         cargarLista()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun cargarListaSemana() {
@@ -59,7 +99,17 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = EventoAdapter(EventosProvider.listaHoy)
     }
 
+    private fun cargarListaGratis(){
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = EventoAdapter(EventosProvider.listaGratis)
+    }
 
+    private fun cargarListaPaga(){
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = EventoAdapter(EventosProvider.listaPaga)
+    }
 
 
 }
--- app/src/main/java/com/example/eventosxalapa/Models/EventosProvider.kt
+++ app/src/main/java/com/example/eventosxalapa/Models/EventosProvider.kt
@@ -28,13 +28,13 @@ class EventosProvider {
                 "01/11/2023",
                 "Festival donde se conmemora este platillo veracruzano tipico",
                 "Ayuntamiento",
-                "https://picsum.photos/id/23/130/130",
+                "https://picsum.photos/id/27/130/130",
                 "Gratis"),
             Evento("Recorrido Cerro de Macuilteptl",
                 "06/11/2023",
                 "Recorrido por todo el cerro de macuilteptl",
                 "Admiistracion del Cerro de Macuilteptl",
-                "https://fastly.picsum.photos/id/786/130/130",
+                "https://picsum.photos/id/27/130/130",
                 "$30")
         )
 
@@ -43,26 +43,59 @@ class EventosProvider {
                 "01/11/2023",
                 "Evento de baile y musica de salsa",
                 "Ayuntamiento",
-                "https://picsum.photos/id/21/130/130",
+                "https://picsum.photos/id/19/130/130",
                 "$60"),
             Evento("Festival del Pambaso",
                 "01/11/2023",
                 "Festival donde se conmemora este platillo veracruzano tipico",
                 "Ayuntamiento",
-                "https://picsum.photos/id/23/130/130",
+                "https://picsum.photos/id/1/130/130",
                 "Gratis"),
             Evento("Recorrido Cerro de Macuilteptl",
                 "06/11/2023",
                 "Recorrido por todo el cerro de macuilteptl",
                 "Admiistracion del Cerro de Macuilteptl",
-                "https://fastly.picsum.photos/id/786/130/130",
+                "https://picsum.photos/id/19/130/130",
                 "$30"),
             Evento("Aniversario del Ayuntamiento de Xalapa",
                 "23/11/2023",
                 "celebracion del aniversario del ayuntamiento de xalapa",
                 "Ayuntamiento",
-                "https://fastly.picsum.photos/id/828/130/130",
+                "https://picsum.photos/id/19/130/130",
                 "Gratis")
         )
+
+        var listaGratis = listOf<Evento>(
+            Evento("Festival del Pambaso",
+                "01/11/2023",
+                "Festival donde se conmemora este platillo veracruzano tipico",
+                "Ayuntamiento",
+                "https://picsum.photos/id/1/130/130",
+                "Gratis"),
+            Evento("Aniversario del Ayuntamiento de Xalapa",
+                "23/11/2023",
+                "celebracion del aniversario del ayuntamiento de xalapa",
+                "Ayuntamiento",
+                "https://picsum.photos/id/19/130/130",
+                "Gratis")
+
+        )
+
+        var listaPaga = listOf<Evento>(
+            Evento("Festival de la Salsa",
+                "01/11/2023",
+                "Evento de baile y musica de salsa",
+                "Ayuntamiento",
+                "https://picsum.photos/id/21/130/130",
+                "$60"),
+            Evento("Recorrido Cerro de Macuilteptl",
+                "06/11/2023",
+                "Recorrido por todo el cerro de macuilteptl",
+                "Admiistracion del Cerro de Macuilteptl",
+                "https://picsum.photos/id/23/130/130",
+                "$30"),
+        )
+
+
     }
 }
